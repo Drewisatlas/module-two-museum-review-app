97a8348e972c586ef820174e2f276821f4b1120c
Drewisatlas <drewisatlas@gmail.com>
2018-11-12 16:46:08 -0500

    added login requirement for a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+
+  def new
+    return head(:forbidden) unless session.include?(:username)
+      @review = Review.new
+  end
+
+  def create
+
+  end
 end
