2cacd6e283f011027868045deacf5c9a30853727
jennyjean <jennyjean8675309@yahoo.com>
2018-11-12 17:33:05 -0500

    added to reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,18 +3,10 @@ class ReviewsController < ApplicationController
   def new
     return head(:forbidden) unless session.include?(:username)
       @review = Review.new
-      @user = current_user.id
+      
   end
 
   def create
-    @review= Review.create(strong_params)
-    @review.user_id = current_user.id
-    @review.save
-    redirect_to review_path(@review)
-  end
 
-private
-  def strong_params
-    params.require(:review).permit(:title, :review, :rating, :museum_id)
   end
 end
