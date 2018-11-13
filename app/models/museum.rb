1bd0f2e5fcdafdcf0b6b5b7fa92709c7044d07a5
jennyjean <jennyjean8675309@yahoo.com>
2018-11-13 11:41:28 -0500

    Created a method to return a museum's most recent reviews

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -2,4 +2,8 @@ class Museum < ApplicationRecord
   belongs_to :museum_type
   has_many :reviews
   has_many :users, through: :reviews
+
+  def most_recent_reviews
+    self.reviews.order("created_at").last(3)
+  end
 end
