a63f492ad2fe811759f004767f663f5cf2741873
jennyjean <jennyjean8675309@yahoo.com>
2018-11-13 15:35:11 -0500

    Created #average_age_of_reviewer and #percentage_by_user_type for museum instances and Review class

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -14,4 +14,19 @@ class Museum < ApplicationRecord
     average = ratings.inject { |sum, el| sum + el }.to_f / ratings.size
     average.round(1)
   end
+
+  def average_age_of_reviewer
+    ages = self.reviews.collect do |review|
+      review.user.age
+    end
+    average_age = ages.inject { |sum, el| sum + el } / ages.size
+  end
+
+  def percentage_by_user_type(user_type)
+    all_of_user_type = self.reviews.select do |review|
+      review.user_type.name == user_type
+    end
+    num = (all_of_user_type.size.to_f / self.reviews.size)
+    "#{(num * 100).round(2)}%"
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,19 @@ class Review < ApplicationRecord
   def shorten_review
     self.review[0..20]+"..."
   end
+
+  def self.average_age_of_reviewer
+    ages = self.all.collect do |review|
+      review.user.age
+    end
+    average_age = ages.inject { |sum, el| sum + el } / ages.size
+  end
+
+  def self.percentage_by_user_type(user_type)
+    all_of_user_type = self.all.select do |review|
+      review.user_type.name == user_type
+    end
+    num = (all_of_user_type.size.to_f / self.all.size)
+    "#{(num * 100).round(2)}%"
+  end
 end
