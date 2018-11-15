db63af857c262e146c60920ed9ea7aa5578d99b6
Drewisatlas <drewisatlas@gmail.com>
2018-11-15 09:58:36 -0500

    Added search engine and site name to the fron page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if Museum.search_by_city(params[:search_by_city]) != nil
+      @museums = Museum.search_by_city(params[:search_by_city])
+    else
+      flash[:notice] = "There are no results for that city"
+      redirect_to root_path
+    end
   end
 end
