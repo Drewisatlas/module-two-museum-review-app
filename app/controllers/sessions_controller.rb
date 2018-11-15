f0a993e5ce975bf1bbd1d3132c155d4ccd1f024a
Drewisatlas <drewisatlas@gmail.com>
2018-11-15 13:38:07 -0500

    added create a profile and login flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,19 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    @user.save
 
-    session[:username] = @user.username
-    session[:user] = @user.id
-
-    redirect_to user_path(@user)
+    if @user && @user.authenticate(params[:password])
+      session[:username] = @user.username
+      session[:user] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Username and password do not match"
+      render :new
+    end
   end
 
   def destroy
     session.delete(:username)
-
     redirect_to museums_path
   end
 
